7420c2234ed4d2f01c028e74ecb8928597729253
MOSUMA <65025326+TDVAppLab@users.noreply.github.com>
2022-10-24 23:01:36 +0900

    Update WeatherForecast.tsx
    refactoring

--- tasket/client-app/src/WeatherForecast.tsx
+++ tasket/client-app/src/WeatherForecast.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type Forecast = {
   date: Date;
@@ -7,62 +7,49 @@ type Forecast = {
   summary: string;
 };
 
-type State = {
-  forecasts: Forecast[];
-  loading: boolean;
-};
+export const WeatherForecast = () => {
+    
+    
+    const [loading, setLoading] = useState(true);
+    const [forecasts, setForecast] = useState<Forecast[]>();
+  
+    useEffect(() => {
+        populateWeatherData();
+    }, []);
+  
+    const populateWeatherData = async () => {
+        const response = await fetch('https://localhost:5001/weatherforecast');
+        const data = await response.json();
+        setForecast(data);
+        setLoading(false);
+    };
+    
+    if(loading) return <div>loading....</div>
 
-export const WeatherForecast: FC = () => {
-  const [state, setState] = useState<State>({ forecasts: [], loading: true });
-
-  useEffect(() => {
-    populateWeatherData();
-  }, []);
-
-  const populateWeatherData = async () => {
-    const response = await fetch('https://localhost:5001/weatherforecast');
-    const data = await response.json();
-    setState({ forecasts: data, loading: false });
-  };
-
-  const renderForecastsTable = (forecasts: Forecast[]) => {
     return (
-      <table className="table table-striped" aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map((forecast) => (
-            <tr key={forecast.date.toString()}>
-              <td>{forecast.date.toString()}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  };
-
-  let contents = state.loading ? (
-    <p>
-      <em>Loading...</em>
-    </p>
-  ) : (
-    renderForecastsTable(state.forecasts)
-  );
-
-  return (
-    <div>
-      <h1 id="tabelLabel">Weather forecast</h1>
-      <p>This component demonstrates fetching data from the server.</p>
-      {contents}
-    </div>
-  );
-};
+        <div>
+            <h1 id="tabelLabel">Weather forecast</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+            <table className="table table-striped" aria-labelledby="tabelLabel">
+                <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Temp. (C)</th>
+                    <th>Temp. (F)</th>
+                    <th>Summary</th>
+                </tr>
+                </thead>
+                <tbody>
+                {forecasts && forecasts.map((forecast) => (
+                    <tr key={forecast.date.toString()}>
+                    <td>{forecast.date.toString()}</td>
+                    <td>{forecast.temperatureC}</td>
+                    <td>{forecast.temperatureF}</td>
+                    <td>{forecast.summary}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
